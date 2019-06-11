279cd846d512d99ca10245c8e1f9f77192a1f082
Ray Kang <adamck6302438@gmail.com>
2019-06-11 17:18:58 -0400

    first commit

---
+++ HeaderStretch/StretchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StretchTableViewCell.swift
+//  HeaderStretch
+//
+//  Created by Frances ZiyiFan on 6/11/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+import UIKit
+
+class StretchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
