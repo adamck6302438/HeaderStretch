a5b763d92df6dac71821c466e6115dd1c44591b5
Ray Kang <adamck6302438@gmail.com>
2019-06-11 17:19:12 -0400

    finished

--- HeaderStretch/StretchTableViewCell.swift
+++ HeaderStretch/StretchTableViewCell.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class StretchTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var categoryLabel : UILabel!
+    @IBOutlet weak var headlineLabel : UILabel!
+    
     override func awakeFromNib() {
+        
         super.awakeFromNib()
-        // Initialization code
+        categoryLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                categoryLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                categoryLabel.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 10),
+                categoryLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0)
+            ]
+        )
+        headlineLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                headlineLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                headlineLabel.topAnchor.constraint(equalTo: categoryLabel.bottomAnchor, constant: 10),
+                headlineLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
+                headlineLabel.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: 0)
+            ]
+        )
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
-
+    
 }
--- HeaderStretch/StretchTableViewController.swift
+++ HeaderStretch/StretchTableViewController.swift
@@ -8,83 +8,112 @@
 
 import UIKit
 
-class StretchTableViewController: UITableViewController {
+struct NewsItem{
+    var category : String
+    var headline : String
+}
 
+private let kTableHeaderHeight = 300.0
+
+class StretchTableViewController: UITableViewController {
+    
+    @IBOutlet weak var headerView : UIView!
+    var newsItems : [NewsItem]!
+    @IBOutlet weak var dateLabel : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //initiates news items
+        newsItems = [
+            NewsItem(category: "World", headline: "Climate change protests, divestments meet fossil fuels realities"),
+            NewsItem(category: "Europe", headline: "Scotland's 'Yes' leader says independence vote is 'once in a lifetime'"),
+            NewsItem(category: "Middle East", headline: "Airstrikes boost Islamic State, FBI director warns more hostages possible"),
+            NewsItem(category: "Africa", headline: "Nigeria says 70 dead in building collapse; questions S. Africa victim claim"),
+            NewsItem(category: "Asia Pacific", headline: "Despite UN ruling, Japan seeks backing for whale hunting"),
+            NewsItem(category: "Americas", headline: "Officials: FBI is tracking 100 Americans who fought alongside IS in Syria"),
+            NewsItem(category: "World", headline: "South Africa in $40 billion deal for Russian nuclear reactors"),
+            NewsItem(category: "Europe", headline: "'One million babies' created by EU student exchanges")
+        ]
+        
+        //add headerview to table view
+        headerView = tableView.tableHeaderView
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.tableHeaderView = nil
+        tableView.addSubview(headerView)
+        tableView.contentInset = UIEdgeInsets(top: CGFloat(kTableHeaderHeight), left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -kTableHeaderHeight)
+        updateHeaderView()
+        //table view auto layout
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: tableView.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: tableView.trailingAnchor)
+                                     ])
+        
+        //auto resize for table view cell
+        tableView.estimatedRowHeight = 100.0
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        //set date label
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d yyyy"
+        let result = formatter.string(from: date as Date)
+        dateLabel.text = result
+        
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 6
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Stretch cell", for: indexPath) as? StretchTableViewCell else{
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        
+        switch newsItems![indexPath.row].category {
+        case "World":
+            cell.categoryLabel.textColor = UIColor.red
+        case "Americas":
+            cell.categoryLabel.textColor = UIColor.blue
+        case "Europe":
+            cell.categoryLabel.textColor = UIColor.green
+        case "Middle East":
+            cell.categoryLabel.textColor = UIColor.yellow
+        case "Africa":
+            cell.categoryLabel.textColor = UIColor.orange
+        case "Asia Pacific":
+            cell.categoryLabel.textColor = UIColor.purple
+        default:
+            cell.categoryLabel.textColor = UIColor.black
+        }
+        
+        cell.categoryLabel.text = newsItems![indexPath.row].category
+        cell.headlineLabel.numberOfLines = 0
+        cell.headlineLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+        cell.headlineLabel.text = newsItems![indexPath.row].headline
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateHeaderView()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func updateHeaderView(){
+        var headerRect = CGRect(x: 0, y: CGFloat(-kTableHeaderHeight), width: tableView.bounds.width, height: CGFloat(kTableHeaderHeight))
+        if tableView.contentOffset.y < CGFloat(-kTableHeaderHeight) {
+            headerRect.origin.y = tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y
+        }
+        headerView.frame = headerRect
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
